e07e381a13d0e5d866dfba99ca5b68b054ad79c7
Satvika S <bl.sc.u4aie24047@bl.students.amrita.edu>
2026-01-29 05:59:59 +0530

    updated

--- LAB2/A4.py
+++ LAB2/A4.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
 
-def load_thyroid_data(filepath):
-    return pd.read_excel(filepath, sheet_name="thyroid0387_UCI")
 
-def calculate_mean_variance(df):
-    numeric_cols = df.select_dtypes(include=np.number)
-    mean = numeric_cols.mean()
-    variance = numeric_cols.var()   
+#calculate mean and variance for all numeric variables
+def mean_variance(df):
+    cols = df.select_dtypes(include=np.number)
+    mean = cols.mean()
+    variance = cols.var()   
     return mean, variance
 
 def main():
+    #loading the excel file
     filepath = "Lab Session Data.xlsx"
-    df = load_thyroid_data(filepath)
+    df = pd.read_excel(filepath, sheet_name="thyroid0387_UCI")
 
-    mean, variance = calculate_mean_variance(df)
+    mean, variance = mean_variance(df)
 
     print("Mean of numeric variables:\n", mean)
     print("\nVariance of numeric variables:\n", variance)
